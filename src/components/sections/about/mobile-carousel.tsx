d40e8f4cd463cf67d196f938fee2d025df6fa756
akbar-dzikri <akbardrzkr05@gmail.com>
2025-09-12 17:02:30 +0700

    feat: enhance MobileCarousel with progress indicator for mobile view
    - Added a progress indicator to the MobileCarousel component that displays based on the current screen size.
    - Utilized useBreakpointBelow hook to determine mobile view and conditionally render the progress component.
    - Improved user experience by providing visual feedback during image transitions.

--- src/components/sections/about/mobile-carousel.tsx
+++ src/components/sections/about/mobile-carousel.tsx
@@ -5,6 +5,9 @@ import {
   CarouselPrevious,
   CarouselNext,
 } from "@/components/ui/carousel";
+import { Progress } from "@/components/ui/progress";
+import { useEffect, useState } from "react";
+import { useBreakpointBelow } from "@/hooks/use-breakpoint";
 
 interface MobileCarouselProps {
   images: {
@@ -14,6 +17,11 @@ interface MobileCarouselProps {
 }
 
 export function MobileCarousel({ images }: MobileCarouselProps) {
+  const [showCarouselProgress, setShowCarouselProgress] = useState(false);
+  const isMobile = useBreakpointBelow("md");
+  useEffect(() => {
+    setShowCarouselProgress(isMobile);
+  }, [isMobile]);
   return (
     <Carousel
       className="relative mx-auto w-full max-w-xs md:hidden"
@@ -37,8 +45,21 @@ export function MobileCarousel({ images }: MobileCarouselProps) {
           );
         })}
       </CarouselContent>
+      <div className="absolute flex w-full justify-center">
+        <MobileCarouselProgress showProgress={showCarouselProgress} />
+      </div>
       <CarouselPrevious className="absolute top-1/2 left-2 -translate-y-1/2" />
       <CarouselNext className="absolute top-1/2 right-2 -translate-y-1/2" />
     </Carousel>
+  );
+}
+
+function MobileCarouselProgress({ showProgress }: { showProgress: boolean }) {
+  return (
+    <>
+      {showProgress ? (
+        <Progress value={50} className="w-full max-w-xs" />
+      ) : null}
+    </>
   );
 }
